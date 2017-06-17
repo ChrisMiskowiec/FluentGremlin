276760878ca53c1b3773bd5a0aa22aec88f471b0
Chris <cmiskow@gmail.com>
2017-06-17 15:30:31 -0400

    Added missing files

--- src/FluentGremlin.Core/GraphTraversal.cs
+++ src/FluentGremlin.Core/GraphTraversal.cs
@@ -1,16 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FluentGremlin.Core
 {
-    public static class GraphTraversalExtensions
+    public static class GraphTraversal
     {
-        public static IGraphTraversal<Vertex> Out(this IGraphTraversal<Vertex> vertex, string edgeLabel)
+        public static IGraphTraversal<Vertex> V(this IGraphTraversalSource source)
         {
-
+            return source.Provider.CreateTraversal<Vertex>(Expression.Call(
+                null,
+                new Func<IGraphTraversalSource, IGraphTraversal<Vertex>>(GraphTraversal.V).GetMethodInfo(),
+                Expression.Constant(source)));
         }
+
+        public static IGraphTraversal<Vertex> Out(this IGraphTraversal<Vertex> source, string edgeLabel)
+        {
+            return source.Provider.CreateTraversal<Vertex>(Expression.Call(
+                null,
+                new Func<IGraphTraversal<Vertex>, string, IGraphTraversal<Vertex>>(GraphTraversal.Out).GetMethodInfo(),
+                source.Expression,
+                Expression.Quote(source.Expression)
+                ));
+        }
+    }
+
+    internal static class CachedReflectionInfo
+    {
+        private static MethodInfo s_Out_1;
+
+        public static MethodInfo Out_1(Type TSource) =>
+             (s_Out_1 ??
+             (s_Out_1 = new Func<IQueryable<object>, Expression<Func<object, object, object>>, object>(Queryable.Aggregate).GetMethodInfo().GetGenericMethodDefinition()))
+              .MakeGenericMethod(TSource);
     }
 }
